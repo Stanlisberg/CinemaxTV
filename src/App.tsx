3e7dd40b670cfbcfe5f052f92866b6fa5e5cb096
Stanlisberg <nnabugo2010@gmail.com>
2023-05-09 02:50:19 +0100

    Feature: added some features.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ function App() {
 
   return (
     <>
+     <div className='app'>
        <Navbar />
        <Sidebar />
+     </div>
     </>
   )
 }
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useState} from 'react'
 import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 import { BiHome } from "react-icons/bi";
 import { BiSearchAlt } from "react-icons/bi";
@@ -10,37 +11,47 @@ import { TbTriangleSquareCircle } from "react-icons/tb";
 // import logo from '../../assets/logo2.png'
 import "../../styles/sidebar.css";
 
+
 function Sidebar() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  const handleSidebar = () => {
+
+    setOpenSidebar(true);
+  }
   return (
     <div>
-      <div className="side-container">
-        <TbTriangleSquareCircle size={50} color="#ad4806" className='logo'/>
-        <div className="side-wrapper">
+      <div className="sidebar-open">
+        <TbTriangleSquareCircle size={50} color="#ad4806" className='logo-open'/>
+        <div className="side-wrapper-open">
           {/* <img src={logo} alt='logo-image' /> */}
-          <div className='icon'>
-            <MdOutlineKeyboardDoubleArrowRight size={25} />
+          <div className='icon-open arrow'>
+            <MdOutlineKeyboardDoubleArrowRight size={25} onClick={handleSidebar}/>
           </div>
-          <div className='icon'>
-            <BiHome size={25} />
-          </div>
-          <div className='icon'>
+          <div className='icon-open'>
             <BiSearchAlt size={25} />
           </div>
-          <div className='icon'>
+          <div className='icon-open'>
+            <BiHome size={25} />
+          </div>
+          <div className='icon-open'>
             <AiOutlineFire size={25} />
           </div>
-          <div className='icon'>
+          <div className='icon-open'>
             <BiCameraMovie size={25} />
           </div>
-          <div className='icon'>
+          <div className='icon-open'>
             <RiSlideshow3Line size={25} />
           </div>
-          <div className='icon'>
+          <div className='icon-open'>
             <BiBookBookmark size={25} />
           </div>
-          <div className='icon setting'>
+          {/* <div className='icon-open setting setting-open'>
             <IoMdSettings size={25} />
-          </div>
+          </div> */}
+        </div>
+        <div className='icon-open setting setting-open'>
+            <IoMdSettings size={25} />
         </div>
       </div>
     </div>
